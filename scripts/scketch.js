06ceb45a103c7b3af8ad2581317324fa2cdb8003
Cuke7 <30923342+Cuke7@users.noreply.github.com>
2018-02-27 16:19:48 +0100

    Zoom ok

--- scripts/scketch.js
+++ scripts/scketch.js
@@ -1,12 +1,8 @@
 // create board
 let cell_dim = 20;
 let board_size="20x40"; let b_height=20; let b_width=40;
-let s_w=b_width*cell_dim
-let s_h=b_height*cell_dim
 let b = jsboard.board({attach:"game", size:board_size});
 b.cell("each").style({width:cell_dim.toString()+"px", height:cell_dim.toString()+"px"});
-document.getElementById("table_wrapper").style.width=s_w.toString()+"px";
-document.getElementById("table_wrapper").style.height=s_h.toString()+"px";
 
 document.getElementById("game").addEventListener("wheel", function(e) {
 	if(e.deltaY < 0){
@@ -18,8 +14,14 @@ document.getElementById("game").addEventListener("wheel", function(e) {
 		if(cell_dim<20)cell_dim=20;
 		b.cell("each").style({width:cell_dim.toString()+"px", height:cell_dim.toString()+"px"});
 	}
-	s_w=b_width*cell_dim
-	s_h=b_height*cell_dim
-	document.getElementById("game").style.width=s_w.toString()+"px";
-	document.getElementById("game").style.height=s_h.toString()+"px";
+	s=cell_dim*b_width+4+b_width*2;
+	s=s.toString();
+	document.getElementById("game").style.width=s+"px";
 });
+
+
+document.getElementById("table_wrapper").style.maxHeight="465px";
+document.getElementById("table_wrapper").style.maxWidth="929px";
+
+var w_knight = jsboard.piece({text:"WK", textIndent:"-9999px", background:"url('images/w_knight.png') no-repeat", width:"auto", height:"auto", margin:"0 auto" });
+b.cell([7,1]).place(w_knight.clone());
