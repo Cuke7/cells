82e4aa7a080ac7c65d3a77fc230e15d0743d58a4
Cuke7 <30923342+Cuke7@users.noreply.github.com>
2018-02-28 12:07:19 +0100

    toto

--- scripts/scketch.js
+++ scripts/scketch.js
@@ -1,21 +1,14 @@
 
-let cell_dim = 20;
-let board_size="20x40"; let b_height=20; let b_width=40;
+let cell_dim = 24;
+let board_size="15x30"; let b_height=25; let b_width=30;
 let b = jsboard.board({attach:"game", size:board_size});
 let table=document.getElementById("game");
 let zoomed=false;
 let init_pos=[table.getBoundingClientRect().x,table.getBoundingClientRect().x];
 
-b.cell("each").style({width:cell_dim.toString()+"px", height:cell_dim.toString()+"px"});
-s=cell_dim*b_height+6+b_height*3;
-s=s.toString();
-document.getElementById("table_wrapper").style.maxHeight=s+"px";
-s=cell_dim*b_width+4+b_width*2;
-s=s.toString();
-document.getElementById("table_wrapper").style.maxWidth=s+"px";
+b.cell("each").style({width:cell_dim.toString()+"px", height:cell_dim.toString()+"px", background:"url('images/basic_tile.png') no-repeat",backgroundSize:"100% 100%"});
 
-
-var w_knight = jsboard.piece({text:"WK", textIndent:"-9999px", background:"url('images/w_knight.png')",backgroundSize:"100% 100%" });
+var w_knight = jsboard.piece({text:"WK", textIndent:"-9999px", background:"url('images/r_wizard.png') no-repeat",backgroundSize:"100% 100%", width:"24px", height:"24px", margin:"0 auto"});
 b.cell([0,0]).place(w_knight.clone());
 
 
@@ -55,6 +48,15 @@ table.addEventListener("wheel", function(e) {
 	s=s.toString();
 	table.style.width=s+"px";
 });
+
+s=cell_dim*b_height+6+b_height*3;
+s=s.toString();
+document.getElementById("table_wrapper").style.maxHeight=s+"px";
+s=cell_dim*b_width+4+b_width*2;
+s=s.toString();
+document.getElementById("table_wrapper").style.maxWidth=s+"px";
+
+
 
 */
 
